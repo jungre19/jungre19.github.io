c785766d4ee59f280304337ba4a577922a300140
杨毅 <498254198@qq.com>
2024-10-24 18:25:39 +0800

    feat: vuepress

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,25 @@
+import * as clientConfig0 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.55_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@_rklo5m4vvjolu7kjyqn5ekzqle/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.56_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_ulxiisdpeueiknfem26276n554/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.56_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.7_si3vxtwz3uwvjyzmg3px4sbze4/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.56_markdown-it@14.1.0_vue@3.5.12_vuepress@2.0.0-rc.18_spmjperc6g4nb2d272u7fh43xy/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.56_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_un24mx7bzztlx65vwifcyp2zpi/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.56_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.7_tysmsufc7wkk5ojzx5zgzfxwwq/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig8 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.56_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc._4t5s2gdvvi3bkpwe7ybcdjk55i/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig9 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.56_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+bundler_nr32bjk25nvm5ztgl4ybyqh62e/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig10 from '/Users/yangyia/Desktop/studySpace/vuepress-starter/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+].map((m) => m.default).filter(Boolean)
---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,12 @@
+import "/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.1.0_vue@3.5.12__vuepress@2.0.0-rc.18_brbpd4bwunoeieixsidihgo6hm/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-prismjs@2.0.0-rc.56_@vueuse+core@11.1.0_vue@3.5.12__vuepress@2.0.0-rc.18_@vu_uypgz2o4s7tlvhfgjoifcom3hm/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.1.0_vue@3.5.12__vuepress@2.0.0-rc.18_brbpd4bwunoeieixsidihgo6hm/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.1.0_vue@3.5.12__vuepress@2.0.0-rc.18_brbpd4bwunoeieixsidihgo6hm/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.1.0_vue@3.5.12__vuepress@2.0.0-rc.18_brbpd4bwunoeieixsidihgo6hm/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "/Users/yangyia/Desktop/studySpace/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.1.0_vue@3.5.12__vuepress@2.0.0-rc.18_brbpd4bwunoeieixsidihgo6hm/node_modules/@vuepress/highlighter-helper/lib/client/composables/collapsedLines.js"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
