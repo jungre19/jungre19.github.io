af2a56dd40120fa26940fbb628c2fa3350b35426
杨毅 <498254198@qq.com>
2024-10-24 18:27:28 +0800

    feat: vuepress

--- docs/.vuepress/.temp/pages/posts/sticky.html.js
+++ docs/.vuepress/.temp/pages/posts/sticky.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/yangyia/Desktop/studySpace/vuepress-starter/docs/.vuepress/.temp/pages/posts/sticky.html.vue"
-const data = JSON.parse("{\"path\":\"/posts/sticky.html\",\"title\":\"Sticky Article\",\"lang\":\"en-US\",\"frontmatter\":{\"date\":\"2021-01-01T00:00:00.000Z\",\"category\":[\"CategoryC\"],\"tag\":[\"tag E\"],\"sticky\":true,\"excerpt\":\"<p>A sticky article demo.</p>\"},\"headers\":[{\"level\":2,\"title\":\"Heading 2\",\"slug\":\"heading-2\",\"link\":\"#heading-2\",\"children\":[{\"level\":3,\"title\":\"Heading 3\",\"slug\":\"heading-3\",\"link\":\"#heading-3\",\"children\":[]}]}],\"git\":{},\"filePathRelative\":\"posts/sticky.md\"}")
+const data = JSON.parse("{\"path\":\"/posts/sticky.html\",\"title\":\"Sticky Article\",\"lang\":\"en-US\",\"frontmatter\":{\"date\":\"2021-01-01T00:00:00.000Z\",\"category\":[\"CategoryC\"],\"tag\":[\"tag E\"],\"sticky\":true,\"excerpt\":\"<p>A sticky article demo.</p>\"},\"headers\":[{\"level\":2,\"title\":\"Heading 2\",\"slug\":\"heading-2\",\"link\":\"#heading-2\",\"children\":[{\"level\":3,\"title\":\"Heading 3\",\"slug\":\"heading-3\",\"link\":\"#heading-3\",\"children\":[]}]}],\"git\":{\"updatedTime\":1729765539000,\"contributors\":[{\"name\":\"杨毅\",\"email\":\"498254198@qq.com\",\"commits\":1}]},\"filePathRelative\":\"posts/sticky.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
